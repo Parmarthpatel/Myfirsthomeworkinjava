eb7fb15df185f9f7c627a3db94cbffd4512de931
paresh patel <paresh19176@yahoo.co.in>
2024-04-22 23:45:58 +0200

    my first gitprogramm commit

---
+++ AutomationtstingJava.java
@@ -0,0 +1,4 @@
+package Listofmanualtopics;
+
+public class AutomationtstingJava {
+}
---
+++ ManualTopics.java
@@ -0,0 +1,71 @@
+package Listofmanualtopics;
+
+public class ManualTopics {
+
+    public static void main(String[] args){
+        System.out.println("manual testing topics");
+        System.out.println("1.what is software?");
+        System.out.println("2.Benefits of Software Testing.");
+        System.out.println("3.Who does Testing?");
+        System.out.println("4.Qualification of Tester.");
+        System.out.println("5.What is manual Testing?");
+        System.out.println("6.What is SDLC?");
+        System.out.println("7.What is STLC?");
+        System.out.println("8.Challenges of Software Testing.");
+        System.out.println("9.Principal of Software Testing.");
+        System.out.println("10.Test Plan and it´s Benefits.");
+        System.out.println("11.Principal of software testing.");
+        System.out.println("12.Test Plan Template");
+        System.out.println("13.Testr Scenario.");
+        System.out.println("14.Why do we write Test Scenario?");
+        System.out.println("15.what is Test Case in software Testing?");
+        System.out.println("16.Why do we write Test Cases?");
+        System.out.println("17.what is Positive Testing?");
+        System.out.println("18. What is Negative Testing");
+        System.out.println("19.Test Scenario Vs Test Case.");
+        System.out.println("20.what do you understand by Software Testing Techniques?");
+        System.out.println("21.What do you understand by Equivalence Common partitioning?(ECP)");
+        System.out.println("22.Can you give me one example of ECP?");
+        System.out.println("23.what do you understand by Boundrgity Value Analysis");
+        System.out.println("24.Can you give one Example of BVA?");
+        System.out.println("25.What do you understand by error guessing?");
+        System.out.println("26.As a Software Tester once you received the requirement,what will be your first approch?");
+        System.out.println("27.What is bug?");
+        System.out.println("28.why are there bug in the software?");
+        System.out.println("29.what is Error, Failure, Bug/Defect");
+        System.out.println("30.Explain Bug life Cycle.");
+        System.out.println("31.What is severity?");
+        System.out.println("32.What is Prioriry?");
+        System.out.println("33.Who decides Severity and who decides Priority?");
+        System.out.println("34.can you give me example of high priority and high severity?");
+        System.out.println("35.What do you understand by Blackbox Testing and whitebox testing?");
+        System.out.println("36.differentiate between blakbox and white bo testing.");
+        System.out.println("37.How many testing are there?");
+        System.out.println("38.what do you undersatnd by system testing?");
+        System.out.println("39.differentiate between Functional Testing and Non-functinal Testing.");
+        System.out.println("40.Differentiate between Smoketesting and Sanity Testing?");
+        System.out.println("41.What do understand by Regression Testing and Re-testing?");
+        System.out.println("42.What is user acceptance testing?");
+        System.out.println("43.what do understand by Static Testing and dynamic testing?");
+        System.out.println("44.what is exploratory testing?");
+        System.out.println("45.when do you use exploratory testing?");
+        System.out.println("46.what is Adhoc testing?");
+        System.out.println("47.When to execute Adgoc Testing?");
+        System.out.println("48.what do you understand by Waterfall Model?");
+        System.out.println("49.when can you use Waterfall Model?");
+        System.out.println("50.What are the advantages and disadvantages(Challenges) of Waterfall Model?");
+        System.out.println("51.what do understand V Model?");
+        System.out.println("52.what are the advantages and disadvantages of V- Model?");
+        System.out.println("53.what do you understand by Agile Model?");
+        System.out.println("54.what are the values of Agile Model?");
+        System.out.println("55.what is Scrum Terminology?");
+        System.out.println("56.what is Agile/Scrum Team?");
+        System.out.println("57.what do you understand by Scrum Framework?");
+        System.out.println("58.Can you tell me types of meetings in Agile Model?");
+        System.out.println("59.what are the advantages of Agile Scrum?");
+        System.out.println("60.What are disadvantages of Agile Scrum?");
+        System.out.println("61.what id definition of DONE?");
+        System.out.println("62.DOR vs DOD");
+
+    }
+}
